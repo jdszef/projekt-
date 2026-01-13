f12de9edc528ed8f851e2b59960617328463cdfb
Cyprian Partyka <129669957+jdszef@users.noreply.github.com>
2026-01-13 12:02:18 +0100

    Add temperature conversion and utility function declarations

--- headerfile.h
+++ headerfile.h
@@ -1,1 +1,9 @@
-
+float FtoC(float F);
+float FtoK(float F);
+float CtoF(float C);
+float CtoK(float C);
+float KtoC(float K);
+float KtoF(float K);
+void historia(double stara, double nowa, char skalastara, char skalanowa);
+int check(float temp, char scale);
+void losowanie();
